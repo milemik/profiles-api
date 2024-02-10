f2b3da3f5e811c0ffb7f6f42426e35f9c3123057
milemik <milemik68@gmail.com>
2024-02-10 11:05:47 +0100

    First commit

---
+++ basic_info/models.py
@@ -0,0 +1,54 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from common.models import BaseUUIDModel
+
+
+class MyBasicInfo(BaseUUIDModel):
+    class WorkStatus(models.TextChoices):
+        OPEN_FOR_WORK = "open-for-work", "Open for work"
+        UNAVAILABLE = "unavailable", "Unavailable"
+        ONLY_SMALL_JOBS = "only-small-jobs", "Can accept only small jobs"
+
+    user = models.OneToOneField(User, related_name="basic_info", on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, null=False, blank=False)
+    last_name = models.CharField(max_length=100, null=False, blank=False)
+    birth_date = models.DateField(null=False, blank=False)
+    gender = models.CharField(max_length=1, null=False, blank=False)
+    current_status = models.CharField(
+        choices=WorkStatus.choices, default=WorkStatus.UNAVAILABLE, max_length=50, null=False, blank=False
+    )
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class SchoolInfo(BaseUUIDModel):
+    class SchoolType(models.IntegerChoices):
+        ELEMENTARY_SCHOOL = 1
+        HIGH_SCHOOL = 2
+        COLLAGE_SCHOOL = 3
+        COURSE = 4
+        OTHER = 5
+
+    basic_info = models.OneToOneField(MyBasicInfo, related_name="school_info", on_delete=models.CASCADE)
+    type = models.IntegerField(choices=SchoolType)
+    school_name = models.CharField(max_length=150, null=True, blank=True)
+    school_town = models.CharField(max_length=100, null=True, blank=True)
+    start_year = models.IntegerField(null=True, blank=True)
+    end_year = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        ordering = ["type", "start_year", "end_year"]
+
+
+class WorkExperience(BaseUUIDModel):
+    basic_info = models.OneToOneField(MyBasicInfo, related_name="work_experience", on_delete=models.CASCADE)
+    company = models.CharField(max_length=100, null=False, blank=False)
+    role = models.CharField(max_length=100, null=False, blank=False)
+    work_description = models.TextField(null=True, blank=True)
+    start_year = models.IntegerField(null=True, blank=True)
+    end_year = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        ordering = ["-start_year"]
