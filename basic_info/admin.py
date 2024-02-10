dd61a28179d80dd5a27d4ed801f97d479ee88c47
milemik <milemik68@gmail.com>
2024-02-10 11:47:13 +0100

    basic_info - registering models in admin.py

--- basic_info/admin.py
+++ basic_info/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from basic_info.models import MyBasicInfo, SchoolInfo, WorkExperience
+
+
+class SchoolInfoInlineAdmin(admin.TabularInline):
+    model = SchoolInfo
+    extra = 1
+
+
+class WorkExperienceInlineAdmin(admin.TabularInline):
+    model = WorkExperience
+    extra = 1
+
+
+@admin.register(MyBasicInfo)
+class MyBasicInfoAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "birth_date")
+    inlines = (SchoolInfoInlineAdmin, WorkExperienceInlineAdmin)
